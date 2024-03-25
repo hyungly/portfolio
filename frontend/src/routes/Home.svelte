f26d7c735437d870b57e35abbf3b1f1c732a0acb
Min <edwardminlee@gmail.com>
2024-03-25 21:19:41 +0900

    rebuild v2.4

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -29,7 +29,14 @@
     $page = 0;
   }
   $: $page, $keyword, get_question_list()
+  
+  let hereKitty = false;
+
+  const handleMouseenter = () => (hereKitty = true);
+  const handleMouseleave = () => (hereKitty = false);
 </script>
+
+<svelte:body on:mouseenter={handleMouseenter} on:mouseleave={handleMouseleave} />
 
 <div class="container my-3">
     <div class="row my-3">
@@ -92,3 +99,29 @@
   </ul>
   <!-- 페이징처리 끝 -->
 </div>
+
+<img
+  class:curious={hereKitty}
+  alt="Kitten wants to know what's going on"
+  src="src\routes\image\kitten.png"
+  style="width: 15%;"
+/>
+
+<style>
+  img {
+      position: absolute;
+      left: 0;
+      bottom: -60px;
+      transform: translate(-80%, 0) rotate(-30deg);
+      transform-origin: 100% 100%;
+      transition: transform 0.4s;
+  } 
+
+  .curious {
+      transform: translate(-15%, 0) rotate(0deg);
+  }
+
+  :global(body) {
+      overflow: hidden;
+  }
+</style>
