d3e1611b454a5f886d8bb49b6833db15e6bf989d
Min <edwardminlee@gmail.com>
2024-03-27 20:30:28 +0900

    rebuild v2.4

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -25,13 +25,7 @@
         })
     }
     $:$page, $keyword, get_question_list()
-    
-    let hereKitty = false;
-    const handleMouseenter = () => (hereKitty = true);
-    const handleMouseleave = () => (hereKitty = false);
 </script>
-
-<svelte:body on:mouseenter={handleMouseenter} on:mouseleave={handleMouseleave} />
 
 <div class="container my-3">
     <div class="row my-3">
@@ -94,27 +88,3 @@
   </ul>
   <!-- 페이징처리 끝 -->
 </div>
-
-<img
-  class:curious={hereKitty}
-  alt="Kitten wants to know what's going on"
-  src="https://www.pngall.com/wp-content/uploads/2016/05/Kitten-Free-PNG-Image.png"
-  style="width: 15%;"
-/>
-
-<style>
-  img {
-      position: absolute;
-      left: 0;
-      bottom: -60px;
-      transform: translate(-80%, 0) rotate(-30deg);
-      transform-origin: 100% 100%;
-      transition: transform 0.4s;
-  } 
-  .curious {
-      transform: translate(-15%, 0) rotate(0deg);
-  }
-  :global(body) {
-      overflow: hidden;
-  }
-</style>
