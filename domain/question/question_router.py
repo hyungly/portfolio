06388293669d20354d5d36d07d25fd29ec45db5b
Min <edwardminlee@gmail.com>
2024-03-27 20:01:19 +0900

    rebuild v2.4

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
 
